157d5448c6e7ac6098ee58a0c613956926ba2475
Harry Harpham <harry@getlantern.org>
2021-04-19 20:06:58 -0600

    Test refactoring

--- ossh_test.go
+++ ossh_test.go
@@ -7,9 +7,9 @@ import (
 	"net"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/net/nettest"
 )
 
 // makePipe implements nettest.MakePipe.
@@ -92,6 +92,11 @@ func makePipe() (c1, c2 net.Conn, stop func(), err error) {
 	return client, server, func() { client.Close(); server.Close() }, nil
 }
 
+func TestConn(t *testing.T) {
+	nettest.TestConn(t, makePipe)
+}
+
+// debugging
 func TestAddr(t *testing.T) {
 	c1, c2, stop, err := makePipe()
 	require.NoError(t, err)
@@ -109,70 +114,4 @@ func TestAddr(t *testing.T) {
 	fmt.Println("c1.Remote:", c1.RemoteAddr())
 	fmt.Println("c2.Local:", c2.LocalAddr())
 	fmt.Println("c2.Remote:", c2.RemoteAddr())
-}
-
-func TestListenAndDial(t *testing.T) {
-	t.Parallel()
-
-	const (
-		clientMsg = "hello from the client"
-		serverMsg = "hello from the server"
-	)
-
-	client, server, stop, err := makePipe()
-	require.NoError(t, err)
-	defer stop()
-
-	type result struct {
-		msg string
-		err error
-	}
-
-	serverResC := make(chan result)
-	clientResC := make(chan result)
-	go func() {
-		msg, err := func() (string, error) {
-			_, err := client.Write([]byte(clientMsg))
-			if err != nil {
-				return "", fmt.Errorf("write error: %w", err)
-			}
-
-			buf := make([]byte, 100)
-			n, err := client.Read(buf)
-			if err != nil {
-				return "", fmt.Errorf("read error: %w", err)
-			}
-			return string(buf[:n]), nil
-		}()
-		clientResC <- result{msg, err}
-	}()
-	go func() {
-		msg, err := func() (string, error) {
-			_, err := server.Write([]byte(serverMsg))
-			if err != nil {
-				return "", fmt.Errorf("write error: %w", err)
-			}
-
-			buf := make([]byte, 100)
-			n, err := server.Read(buf)
-			if err != nil {
-				return "", fmt.Errorf("read error: %w", err)
-			}
-			return string(buf[:n]), nil
-		}()
-		serverResC <- result{msg, err}
-	}()
-
-	for i := 0; i < 2; i++ {
-		select {
-		case res := <-clientResC:
-			if assert.NoError(t, res.err) {
-				assert.Equal(t, serverMsg, res.msg)
-			}
-		case res := <-serverResC:
-			if assert.NoError(t, res.err) {
-				assert.Equal(t, clientMsg, res.msg)
-			}
-		}
-	}
 }
